588bc10687fd2cac9f81b917fc2f3c94939f4133
amitkrmaharana <ak657harley@gmail.com>
2021-02-08 22:37:51 +0530

    Refactor: compared lengths of two lines

--- lineComparision.java
+++ lineComparision.java
@@ -4,12 +4,20 @@ public class lineComparision {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparision Computation Program");
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter the values of the Co-ordinates as follows: x1 y1 x2 y2");
-		double x1 = scan.nextDouble();
-		double y1 = scan.nextDouble();
-		double x2 = scan.nextDouble();
-		double y2 = scan.nextDouble();
-		double length = Math.sqrt(Math.pow((x2-x1),2) + Math.pow((y2-y1),2));
-		System.out.println("Length of the line formed between the two co-ordinates is: " +length);
+		System.out.println("Enter the values of the Co-ordinates for 2 lines as follows: x1 y1 x2 y2 for line1 as well as for line2");
+		double line1_x1 = scan.nextDouble();
+		double line1_y1 = scan.nextDouble();
+		double line1_x2 = scan.nextDouble();
+		double line1_y2 = scan.nextDouble();
+		double line2_x1 = scan.nextDouble();
+                double line2_y1 = scan.nextDouble();
+                double line2_x2 = scan.nextDouble();
+                double line2_y2 = scan.nextDouble();
+                double length_line1 = Math.sqrt(Math.pow((line1_x2-line1_x1),2) + Math.pow((line1_y2-line1_y1),2));
+		double length_line2 = Math.sqrt(Math.pow((line2_x2-line2_x1),2) + Math.pow((line2_y2-line2_y1),2));
+		if (length_line1 == length_line2)
+			System.out.println("Both lines have equal lenngths");
+		else
+			System.out.println("Both lines have different lengths");
 	}
 }
