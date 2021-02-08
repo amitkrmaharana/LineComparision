237a5ceffcac7cb538c3fb09a516284149190139
amitkrmaharana <ak657harley@gmail.com>
2021-02-08 23:23:24 +0530

    Refactor: compared the length of two lines

--- lineComparision.java
+++ lineComparision.java
@@ -16,8 +16,10 @@ public class lineComparision {
                 double length_line1 = Math.sqrt(Math.pow((line1_x2-line1_x1),2) + Math.pow((line1_y2-line1_y1),2));
 		double length_line2 = Math.sqrt(Math.pow((line2_x2-line2_x1),2) + Math.pow((line2_y2-line2_y1),2));
 		if (length_line1 == length_line2)
-			System.out.println("Both lines have equal lenngths");
+			System.out.println("Both lines have equal lengths");
+		else if (length_line1 > length_line2)
+			System.out.println("Length of line1 with length "+length_line1+" is GREATER than Length of line2 with length "+length_line2);
 		else
-			System.out.println("Both lines have different lengths");
+			System.out.println("Length of line1 with length "+length_line1+" is LESSER than Length of line2 with length "+length_line2);
 	}
 }
